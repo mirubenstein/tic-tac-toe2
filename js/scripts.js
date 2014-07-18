b164d98d64cae38176b9c5f10a16fcb34684a5bd
Mark Rubenstein & Scott Dudley <student@epicodus.com>
2014-07-18 14:52:53 -0700

    Finished (we think) logic and specs

--- js/scripts.js
+++ js/scripts.js
@@ -10,20 +10,18 @@ var Player = {
 };
 
 var Space = {
-  initialize: function(xCoord, yCoord){
-    this.xCoordinate = xCoord;
-    this.yCoordinate = yCoord;
+  initialize: function(xCoordinate, yCoordinate){
+    this.xCoordinate = xCoordinate;
+    this.yCoordinate = yCoordinate;
+    // this.markedBy = "";
   },
-  create: function(xCoord, yCoord){
+  create: function(xCoordinate, yCoordinate){
     var newSpace = Object.create(Space);
-    newSpace.initialize(xCoord, yCoord);
+    newSpace.initialize(xCoordinate, yCoordinate);
     return newSpace;
   },
-  markBy: function(playerMarking){
-    this.markedBy = playerMarking;
-  }
-  find: function(xCoord, yCoord){
-
+  markBy: function(player){
+    this.markedBy = player.symbol;
   }
 };
 
@@ -35,5 +33,61 @@ var Board = {
         this.spaces.push(Space.create(i,j));
       }
     }
+  },
+  create: function() {
+    var newBoard = Object.create(Board);
+    newBoard.initialize();
+    return newBoard;
+  },
+  find: function(xCoordinate, yCoordinate){
+    var foundSpace;
+    this.spaces.forEach(function(space){
+      if (space.xCoordinate === xCoordinate && space.yCoordinate === yCoordinate){
+        foundSpace = space;
+      }
+    });
+    return foundSpace
+  },
+  win: function(playerSymbol){
+    var playerWon = false;
+    for (var i = 1; i < 4; i++){
+      if( playerSymbol === this.find(i,1).markedBy &&
+          this.find(i,1).markedBy === this.find(i,2).markedBy &&
+          this.find(i,1).markedBy === this.find(i,3).markedBy) {
+        playerWon = true;
+      }
+    }
+    for (var i = 1; i < 4; i++){
+      if( playerSymbol === this.find(1, i).markedBy &&
+          this.find(1, i).markedBy === this.find(2, i).markedBy &&
+          this.find(1, i).markedBy === this.find(3, i).markedBy) {
+        playerWon = true;
+      }
+    }
+    if (playerSymbol === this.find(1,1).markedBy &&
+        this.find(1,1).markedBy === this.find(2,2).markedBy &&
+        this.find(1,1).markedBy === this.find(3,3).markedBy){
+      playerWon = true;
+    }
+    if (playerSymbol === this.find(1,3).markedBy &&
+        this.find(1,3).markedBy === this.find(2,2).markedBy &&
+        this.find(1,3).markedBy === this.find(3,1).markedBy){
+      playerWon = true;
+    }
+    return playerWon;
   }
 };
+
+var Game = {
+  initialize: function() {
+    this.turn = 1;
+    this.player1 = Player.create("X");
+    this.player2 = Player.create("Y");
+    this.board = Board.create();
+  },
+  create: function(){
+    var newGame = Object.create(Game);
+    newGame.initialize();
+    return newGame;
+  }
+}
--- spec/specs.js
+++ spec/specs.js
@@ -41,17 +41,83 @@ describe("Space", function() {
       var testPlayer = Player.create("X");
       var testSpace = Space.create(1, 2);
       testSpace.markBy(testPlayer);
-      testSpace.markedBy.should.equal(testPlayer);
+      testSpace.markedBy.should.equal("X");
     });
   });
 });
 
 describe("Board", function() {
-  it("creates 9 spaces when it is initialized", function(){
-    var testBoard = Object.create(Board);
-    testBoard.initialize();
-    console.log(testBoard.spaces);
-    testBoard.spaces[0].xCoordinate.should.eql(1);
-    testBoard.spaces[0].yCoordinate.should.eql(1);
-  }); // you write the rest!
+  describe("initialize", function() {
+    it("creates 9 spaces when it is initialized", function(){
+      var testBoard = Object.create(Board);
+      testBoard.initialize();
+      testBoard.spaces[0].xCoordinate.should.eql(1);
+      testBoard.spaces[0].yCoordinate.should.eql(1);
+    });
+  });
+  describe("creates", function() {
+    it("creates a Board object", function(){
+      var testBoard = Board.create();
+      Board.isPrototypeOf(testBoard).should.equal(true);
+      testBoard.spaces[4].xCoordinate.should.equal(2);
+      testBoard.spaces[4].yCoordinate.should.equal(2);
+    });
+  });
+  describe("find", function() {
+    it("finds and returns a space object based on coordinates", function(){
+      var testBoard = Board.create();
+      testBoard.find(2,2).should.equal(testBoard.spaces[4]);
+    });
+  });
+  describe("win", function() {
+    it("ends game if three horizontal spaces are marked by the same symbol", function(){
+      var testBoard = Board.create();
+      var testPlayer = Player.create("X");
+      testBoard.spaces[5].markBy(testPlayer);
+      testBoard.spaces[4].markBy(testPlayer);
+      testBoard.spaces[3].markBy(testPlayer);
+      testBoard.win("X").should.equal(true);
+    });
+   });
+  describe("win", function(){
+    it("ends game if three vertical spaces are marked by the same symbol", function(){
+      var testBoard = Board.create();
+      var testPlayer = Player.create("X");
+      testBoard.spaces[1].markBy(testPlayer);
+      testBoard.spaces[4].markBy(testPlayer);
+      testBoard.spaces[7].markBy(testPlayer);
+      testBoard.win("X").should.equal(true);
+    });
+  });
+  describe("win", function(){
+    it("ends game if three diagonal spaces are marked by the same symbol", function(){
+      var testBoard = Board.create();
+      var testPlayer = Player.create("X");
+      testBoard.spaces[0].markBy(testPlayer);
+      testBoard.spaces[4].markBy(testPlayer);
+      testBoard.spaces[8].markBy(testPlayer);
+      testBoard.win("X").should.equal(true);
+    });
+  });
+});
+
+describe("Game", function(){
+  describe("initialize", function(){
+    it("initializes a new game with a clear board, 2 players, and sets turn to 1", function(){
+      var testGame = Object.create(Game);
+      testGame.initialize();
+      testGame.player1.symbol.should.equal("X");
+      testGame.board.spaces[0].xCoordinate.should.eql(1);
+      testGame.turn.should.equal(1);
+    });
+  });
+  describe("create", function(){
+    it("create a new game object from Game prototype", function(){
+      var testGame = Game.create();
+      Game.isPrototypeOf(testGame).should.equal(true);
+      testGame.player1.symbol.should.equal("X");
+      testGame.board.spaces[0].xCoordinate.should.eql(1);
+      testGame.turn.should.equal(1);
+    });
+  });
 });
